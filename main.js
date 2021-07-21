87efd3611d9f6a709cb605782ace154fdf25ad5e
Yasya <80125891+yasya-ayesha@users.noreply.github.com>
2021-07-21 08:11:54 +0300

    Add files via upload
    improved title code

---
+++ main.js
@@ -0,0 +1,137 @@
+/*
+new Swiper('.swiper-container', {
+	loop: true,
+	navigation: {
+		nextEl: '.arrow',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20
+		},
+		541: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+	menuButton.classList.toggle('menu-button-active');
+	menu.classList.toggle('header-active');
+})
+*/
+
+const getElement = (tagName, classNames, attributes) => {
+	const element = document.createElement(tagName);
+
+	if (classNames) {
+		element.classList.add(...classNames);
+	}
+
+	if (attributes) {
+		for (const attribute in attributes) {
+			element[attribute] = attributes[attribute];
+		}
+	}
+
+	return element;
+};
+
+const createHeader = (param) => {
+	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['header']);
+
+	if (param.header.logo) {
+		const logo = getElement('img', ['logo'], {
+			src: param.header.logo,
+			alt: 'logo ' + param.title,
+		});
+		wrapper.append(logo);
+	}
+
+	if (param.header.menu) {
+		// homework
+		const menuWrapper = getElement('div', ['menu']);
+		const allMenu = param.header.menu.map(item => {
+			const menuLink = getElement('a', ['menu-link']);
+			menuLink.append(item.title);
+			menuLink.href = item.link;
+			return menuLink;
+		});
+
+		menuWrapper.append(...allMenu);
+		wrapper.append(menuWrapper);
+	}
+
+	if (param.header.social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = param.header.social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title,
+			}));
+			socialLink.href = item.link;
+			return socialLink;
+		});
+
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper);
+	}
+
+	header.append(container);
+	container.append(wrapper);
+
+	return header;
+};
+
+const movieConstructor = (selector, options) => {
+	document.title = options.title;
+	const app = document.querySelector(selector);
+	app.classList.add('body-app');
+	if (options.header) {
+		app.append(createHeader(options));
+	}
+};
+
+movieConstructor('.app', {
+	title: 'Ведьмак',
+	header: {
+		logo: 'witcher/logo.png',
+		social: [
+			{
+				title: 'Twitter',
+				link: 'https://twitter.com',
+				image: 'witcher/social/twitter.svg',
+			},
+			{
+				title: 'Instagram',
+				link: 'https://instagram.com',
+				image: 'witcher/social/instagram.svg',
+			},
+			{
+				title: 'Facebook',
+				link: 'https://facebook.com',
+				image: 'witcher/social/facebook.svg',
+			}
+		],
+		menu: [
+			{
+				title: 'Описание',
+				link: '#',
+			},
+			{
+				title: 'Трейлер',
+				link: '#',
+			},
+			{
+				title: 'Отзывы',
+				link: '#',
+			},
+		],
+	},
+});
